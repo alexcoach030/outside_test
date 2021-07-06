9720abade276e90f63cb09210334af8a9ac85d33
alexcoach <mvdrf030@gmail.com>
2021-07-06 18:52:24 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <PopupComponent/>
+  </div>
+</template>
+
+<script>
+
+import PopupComponent from "@/components/PopupComponent.vue";
+
+export default {
+  name: 'App',
+  components: {
+    PopupComponent
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PopupComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PopupComponent",
+  data(){
+    return{
+      message: 'popup'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
