0bbce6763ea879c66a3b91b28eaf0fe4f94add5f
alexcoach <mvdrf030@gmail.com>
2021-07-07 02:21:20 +0200

    done let's do calc

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
     <PopupComponent/>
   </div>
 </template>
@@ -18,12 +18,21 @@ export default {
 </script>
 
 <style lang="less">
+body{
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  background: linear-gradient(255.35deg, #DC3131 0.83%, rgba(255, 79, 79, 0) 108.93%), #FF5E56;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  font-family: Lab G, sans-serif;
+  display: flex;
+  justify-content: space-around;
+  height: 100vh;
+  width: 100vw;
 }
 </style>
--- src/components/PopupComponent.vue
+++ src/components/PopupComponent.vue
@@ -1,6 +1,37 @@
 <template>
-  <div>
-    <p>{{ message }}</p>
+  <div class="main">
+    <div class="content" v-if="!showContent">
+      <button class="button__start" @click="switchPopup()">Налоговый вычет</button>
+    </div>
+    <div class="default" v-if="showContent">
+      <div class="default__container">
+        <div class="default__info">
+          <button class="button__closing" @click="switchPopup()"><img src="../assets/closing.svg" alt="close_popup"></button>
+          <h1 class="default__name">Налоговый вычет</h1>
+          <p class="default__description">Используйте налоговый вычет чтобы погасить ипотеку досрочно. Размер налогового вычета составляет не более 13% от своего официального годового дохода.</p>
+        </div>
+        <div class="default__salary">
+          <h2 class="default__text">Ваша зарплата в месяц</h2>
+          <form @submit.prevent="">
+            <input class="default__textinput" type="number" min="0" placeholder="Введите данные" v-model="salary">
+            <button class="default__submit" @click="calculate()">Рассчитать</button>
+          </form>
+        </div>
+        <div v-if="!calculated" class="default__reduce">
+          <h3 class="default__text">Что уменьшаем?</h3>
+          <button @click="event => switchReduce(event)" value="payment" class="reduce__button" v-bind:class="{ 'reduce__button_active':this.paymentReduce }">Платеж</button>
+          <button @click="event => switchReduce(event)" value="term" class="reduce__button" v-bind:class="{ 'reduce__button_active': !this.paymentReduce }">Срок</button>
+        </div>
+        <div v-if="calculated" class="calculated">
+          <p>Итого можете внести в качестве досрочных:</p>
+          <p>{{ deduction }}</p>
+
+        </div>
+        <div>
+          <button class="default__addbutton">Добавить</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,12 +40,180 @@ export default {
   name: "PopupComponent",
   data(){
     return{
-      message: 'popup'
+      message: 'popup',
+      showContent: true,
+      salary: '',
+      paymentReduce: true,
+      calculated: '',
+      deduction: 0,
+    }
+  },
+  methods:{
+    switchPopup(){
+      this.paymentReduce = false;
+      return this.showContent = !this.showContent;
+    },
+    switchReduce(event){
+      let value = event.target.value;
+      if (value === 'payment'){
+        return this.paymentReduce = true;
+      }else return this.paymentReduce = false;
+    },
+    calculate(){
+      if (this.salary !== ''){
+        let salary = this.salary;
+        let deduction = salary*12*0.13;
+        this.calculated = true;
+        this.deduction = deduction;
+      }
     }
   }
 }
 </script>
 
 <style scoped lang="less">
-
+  .main{
+    width: 100%;
+  }
+  .content{
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .default{
+    .content;
+    background: #E5E5E5;
+    width: 100%;
+    flex-direction: column;
+  }
+  .button__closing{
+    .button;
+    position: absolute;
+    left: 94%;
+    top: -3%;
+    color: #EA0029;
+    background-color: transparent;
+    border: none;
+    &:hover{
+      img{
+        transform: scale(1.5);
+        transition-duration: 0.5s;
+      }
+    }
+  }
+  .default__container{
+    width: 50%;
+    background-color: #FFFFFF;
+    padding: 32px 16px 16px 16px;
+    text-align: left;
+  }
+  .default__text{
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 24px;
+    color: #000000;
+    margin: 0;
+  }
+  .default__info{
+    position: relative;
+    margin-bottom: 24px;
+  }
+  .default__name{
+    margin: 0 0 16px 0;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 24px;
+    color: #000000;
+  }
+  .default__description{
+    font-size: 12px;
+    line-height: 16px;
+    color: #808080;
+    margin-bottom: 0;
+  }
+  .default__reduce{
+    margin-top: 24px;
+  }
+  .default__textinput{
+    margin: 8px 0;
+    padding: 8px 10px;
+    width: 95%;
+    line-height: 24px;
+    font-size: 14px;
+    border: 1px solid #DFE3E6;
+    color: #000000;
+    outline: none;
+    &:hover{
+      border: 1px solid #000000;
+    }
+    &:focus{
+      border: 1px solid #DFE3E6;
+    }
+  }
+  .default__submit{
+    .default__text;
+    color: #EA0029!important;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+    padding: 0;
+  }
+  .button{
+    outline: none;
+    cursor: pointer;
+  }
+  .button__classic{
+    .button;
+    background: linear-gradient(255.35deg, #DC3131 0.83%, rgba(255, 79, 79, 0) 108.93%), #FF5E56;
+    box-shadow: 0 0 24px rgba(234, 0, 41, 0.33);
+    border-radius: 6px;
+    color: #FFFFFF;
+    font-weight: 500;
+    border: none;
+    &:hover{
+      background: #EA0029;
+    }
+    &:active{
+      background: #EA0029;
+    }
+  }
+  .button__classic_big{
+    .button__classic;
+    width: 280px;
+    height: 56px;
+    font-size: 16px;
+    line-height: 24px;
+  }
+  .button__classic_small{
+    .button__classic;
+    height: 40px;
+    width: 288px;
+    font-size: 12px;
+    line-height: 16px;
+  }
+  .button__start{
+    .button__classic_big;
+    border: 1px outset white;
+    &:active{
+      border-style: solid;
+    }
+  }
+  .reduce__button{
+    .button;
+    .default__text;
+    border-radius: 50px;
+    background: #EEF0F2;
+    border: none;
+    padding: 7px 12px;
+    margin: 30px 8px 140px 0;
+  }
+  .reduce__button_active{
+    background: linear-gradient(255.35deg, #DC3131 0.83%, rgba(255, 79, 79, 0) 108.93%), #FF5E56;
+    color: #FFFFFF;
+  }
+  .default__addbutton{
+    .button__classic_small;
+    width: 100%;
+  }
 </style>
